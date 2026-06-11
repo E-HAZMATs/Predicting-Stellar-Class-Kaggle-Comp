c145e3eedfdac1c43679b88a233b93eba5bafd3d
eHAZMATs <alzamarkal@gmail.com>
2026-06-11 13:52:04 +0300

    Predicting Stellar Classes with Decision Tree.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,2 +1,28 @@
-def pre_process():
-    pass
+import pandas as pd
+def pre_process(data):
+    data = data.copy()
+
+    # ==== Encoding object dtype columns to numbers ==== 
+    
+    # galaxy_population encoding
+    data['galaxy_population'] = data['galaxy_population'].map({'Red_Sequence': 0, 'Blue_Cloud': 1}) # Should be float?
+    
+    # Spectral Type OHE
+    data = pd.get_dummies(data, columns=['spectral_type'], dtype=int)
+    
+    # "Class" encoding.
+    if 'class' in data.columns:
+        data['class'] = data['class'].map({'GALAXY': 0, 'QSO': 1, 'STAR': 2})
+
+    # ====
+
+    # 
+
+    return data
+
+
+def get_x_y(data):
+    x = data.drop(columns=['id', 'class'])
+    y = data['class']
+
+    return x, y
